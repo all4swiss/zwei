26e661bdac76ace08af033ffbeeec25cd782feeb
all4swiss <all4swiss@gmail.com>
2026-08-25 21:36:49 +0200

    Clear stale Zwei caches so Safari gets the new design

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE = "zwei-v3";
-const FILES = ["./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
-self.addEventListener("install", e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener("install", (e) => self.skipWaiting());
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
 });
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request, { cache: "no-store" }).catch(() => caches.match(e.request)));
 });
